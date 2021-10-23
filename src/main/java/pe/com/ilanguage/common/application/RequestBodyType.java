866651ecb2a3ad1f3a23709b88c2ae5642549ec9
SebTou <sebaas14toulier@gmail.com>
2021-10-23 13:13:03 -0500

    feat: first commit

---
+++ src/main/java/pe/com/ilanguage/common/application/RequestBodyType.java
@@ -0,0 +1,14 @@
+package pe.com.ilanguage.common.application;
+
+public enum RequestBodyType {
+    VALID {
+        public String toString() {
+            return "VALID";
+        }
+    },
+    INVALID {
+        public String toString() {
+            return "INVALID";
+        }
+    },
+}
---
+++ src/main/java/pe/com/ilanguage/common/application/ResultType.java
@@ -0,0 +1,6 @@
+package pe.com.ilanguage.common.application;
+
+public enum ResultType {
+    SUCCESS,
+    FAILURE
+}
